75c696ac383605c5a0e450f23b8773d4f96c1a97
Zach <zjbu224@uky.edu>
2020-10-29 16:51:24 -0400

    q4

--- core/src/main/java/jenkins/model/IdStrategy.java
+++ core/src/main/java/jenkins/model/IdStrategy.java
@@ -246,37 +246,21 @@ public abstract class IdStrategy extends AbstractDescribableImpl<IdStrategy> imp
                         }
                     } else if (c == '$') {
                         StringBuilder hex = new StringBuilder(4);
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
+                        boolean flag = false;
+                        for (int j = 0; j < 4; j++){
+                            if (j < chars.length)
+                                hex.append(chars[j]);
+                            else
+                                flag = true;
                         }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
+                        if (flag)
                             break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
-                        i++;
-                        if (i < chars.length) {
-                            hex.append(chars[i]);
-                        } else {
-                            break;
-                        }
                         buf.append(Character.valueOf((char)Integer.parseInt(hex.toString(), 16)));
                     }
                 }
                 return buf.toString();
             }
         }
-
         /**
          * {@inheritDoc}
          */
