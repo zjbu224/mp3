2f908c777d519eb5365f7e6bf35040ad446f8713
Zach <zjbu224@uky.edu>
2020-10-29 17:17:17 -0400

    q5

--- cli/src/main/java/hudson/cli/Connection.java
+++ cli/src/main/java/hudson/cli/Connection.java
@@ -53,7 +53,8 @@ import java.security.Signature;
 import java.security.interfaces.DSAPublicKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.X509EncodedKeySpec;
-
+import java.security.NoSuchAlgorithmException;
+import java.security.InvalidAlgorithmParameterException;
 public class Connection {
     public final InputStream in;
     public final OutputStream out;
@@ -152,21 +153,14 @@ public class Connection {
         if (side) {
             AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
             paramGen.init(keySize);
-
-            KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
-            dh.initialize(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
-            keyPair = dh.generateKeyPair();
+            keyPair = generateKeyPairWithSpec(paramGen.generateParameters().getParameterSpec(DHParameterSpec.class));
 
             // send a half and get a half
             writeKey(keyPair.getPublic());
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
         } else {
             otherHalf = KeyFactory.getInstance("DH").generatePublic(readKey());
-
-            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DH");
-            keyPairGen.initialize(((DHPublicKey) otherHalf).getParams());
-            keyPair = keyPairGen.generateKeyPair();
-
+            keyPair = generateKeyPairWithSpec(((DHPublicKey) otherHalf).getParams());
             // send a half and get a half
             writeKey(keyPair.getPublic());
         }
@@ -260,5 +254,13 @@ public class Connection {
     public void close() throws IOException {
         in.close();
         out.close();
+    }
+
+    private KeyPair generateKeyPairWithSpec(DHParameterSpec parameterSpec) throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        KeyPair keyPair;
+        KeyPairGenerator dh = KeyPairGenerator.getInstance("DH");
+        dh.initialize(parameterSpec);
+        keyPair = dh.generateKeyPair();
+        return keyPair;
     }
 }
